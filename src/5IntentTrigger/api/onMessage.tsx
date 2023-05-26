2cf83beef864fb3f53b85e0d70689b71acbca1dd
mathiscaprin <mathis.caprin@iadvize.com>
2023-05-26 11:47:00 +0200

    updated onMessage of 5IntentTrigger

--- src/5IntentTrigger/api/onMessage.tsx
+++ src/5IntentTrigger/api/onMessage.tsx
@@ -2,9 +2,24 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import Cors from 'cors'
 import { coffees } from './products'
 
-//const cors
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
 
-//function runmiddleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+ ) {
+  return new Promise((resolve, reject) => {
+   fn(req, res, (result: any) => {
+     if (result instanceof Error) {
+       return reject(result)
+     }
+     return resolve(result)
+    })
+  })
+ }
 
 type Message = {
 	id : string,
@@ -30,24 +45,24 @@ type Commande = {
 
 export default async function handler(req : NextApiRequest,res : NextApiResponse){
 
-    //call middleware
+    runMiddleware(req,res,cors) //run the middleware to avoid cors Errors
   
     const commandes : Commande[] = []
-    const messages = req.body.messages //your messages
-    messages.forEach((message : Message) => {
-        coffees.forEach(coffee =>{
-            if (message.text.includes(coffee.name)){
-                commandes.push(
+    const messages = req.body.messages 
+    messages.forEach((message : Message) => { //for every messages
+        coffees.forEach(coffee =>{              //for every coffee
+            if (message.text.includes(coffee.name)){ //if the coffee is in the message
+                commandes.push(   //add the following commande to commands
                   {
-                    "type": "addMessageActions",
-                    "messageId": message.id,
+                    "type": "addMessageActions", 
+                    "messageId": message.id, //the id of the message
                     "actions": [
                       {
-                        "highlight": coffee.name,
-                        "intent": {
-                          "key": coffee.id.toString(),
+                        "highlight": coffee.name, //highlight the name of the coffee in the message
+                        "intent": { 
+                          "key": coffee.id.toString(), //the key is the id of the coffee
                           "payload": {
-                            any : coffee,
+                            any : coffee, //pass the coffee object to the custom app
                           }
                         }
                       }
@@ -57,7 +72,7 @@ export default async function handler(req : NextApiRequest,res : NextApiResponse
               }
         })
     });
-    /*res.send({
-        "commands" : commandes
-      })*/
+    res.send({
+        "commands" : commandes //send the array of command to the custom app
+    })
 }
